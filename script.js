e6956fc3597fda025bdca38c562e4d905740b5c6
Horuslllll <mailhar@yandex.ru>
2017-07-15 01:27:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-alert("hello");
-var arr={
-
-};
-function run(idv) {
-document.getElementById(idv).style="height:180px;margin-top:20px";
-
-document.getElementById(idv).src="http://martinkingauthor.com/img/home-jh1-3.png";
-var image =createElement("<img>");
-image.src="http://oi59.tinypic.com/292vbjc.jpg";
-document.getElementById(id).parentNode.appendChild(image);
-};
+function createParagraph() {
+  var para = document.createElement('p');
+  para.textContent = 'You clicked the button!';
+  document.body.appendChild(para);
+}
