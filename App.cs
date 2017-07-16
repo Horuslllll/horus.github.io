b3f408bb6f6c22e9e807f458e017ed2f8acd68e0
Horuslllll <mailhar@yandex.ru>
2017-07-16 18:35:26 +0300

    Add files via upload

---
+++ App.cs
@@ -0,0 +1,13 @@
+using Bridge;
+using Bridge.Html5;
+
+namespace Demo
+{
+    public class App
+    {
+        public static void Main()
+        {
+            // How to call notify()?
+        }
+    }
+}
